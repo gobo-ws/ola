ec7b743d0ba18b2c5838ef53385eecd2927596d8
Johan Nilsson <jn@gobo.ws>
2018-02-23 10:10:41 +0100

    Update Makefile.mk

--- plugins/hue/Makefile.mk
+++ plugins/hue/Makefile.mk
@@ -1,39 +1,39 @@
 # LIBRARIES
 ##################################################
-if USE_NANOLEAF
+if USE_HUE
 # This is a library which isn't coupled to olad
-noinst_LTLIBRARIES += plugins/nanoleaf/libolananoleafnode.la
-plugins_nanoleaf_libolananoleafnode_la_SOURCES = plugins/nanoleaf/NanoleafNode.cpp \
-                                                 plugins/nanoleaf/NanoleafNode.h
+noinst_LTLIBRARIES += plugins/hue/libolananoleafnode.la
+plugins_nanoleaf_libolananoleafnode_la_SOURCES = plugins/hue/NanoleafNode.cpp \
+                                                 plugins/hue/NanoleafNode.h
 plugins_nanoleaf_libolananoleafnode_la_LIBADD = common/libolacommon.la
 
-lib_LTLIBRARIES += plugins/nanoleaf/libolananoleaf.la
+lib_LTLIBRARIES += plugins/hue/libolananoleaf.la
 
 # Plugin description is generated from README.md
-built_sources += plugins/nanoleaf/NanoleafPluginDescription.h
+built_sources += plugins/hue/NanoleafPluginDescription.h
 nodist_plugins_nanoleaf_libolananoleaf_la_SOURCES = \
-    plugins/nanoleaf/NanoleafPluginDescription.h
-plugins/nanoleaf/NanoleafPluginDescription.h: plugins/nanoleaf/README.md plugins/nanoleaf/Makefile.mk plugins/convert_README_to_header.sh
-	sh $(top_srcdir)/plugins/convert_README_to_header.sh $(top_srcdir)/plugins/nanoleaf $(top_builddir)/plugins/nanoleaf/NanoleafPluginDescription.h
+    plugins/hue/NanoleafPluginDescription.h
+plugins/hue/NanoleafPluginDescription.h: plugins/hue/README.md plugins/hue/Makefile.mk plugins/convert_README_to_header.sh
+	sh $(top_srcdir)/plugins/convert_README_to_header.sh $(top_srcdir)/plugins/nanoleaf $(top_builddir)/plugins/hue/NanoleafPluginDescription.h
 
 plugins_nanoleaf_libolananoleaf_la_SOURCES = \
-    plugins/nanoleaf/NanoleafPlugin.cpp \
-    plugins/nanoleaf/NanoleafPlugin.h \
-    plugins/nanoleaf/NanoleafDevice.cpp \
-    plugins/nanoleaf/NanoleafDevice.h \
-    plugins/nanoleaf/NanoleafPort.h
+    plugins/hue/NanoleafPlugin.cpp \
+    plugins/hue/NanoleafPlugin.h \
+    plugins/hue/NanoleafDevice.cpp \
+    plugins/hue/NanoleafDevice.h \
+    plugins/hue/NanoleafPort.h
 plugins_nanoleaf_libolananoleaf_la_LIBADD = \
     olad/plugin_api/libolaserverplugininterface.la \
-    plugins/nanoleaf/libolananoleafnode.la
+    plugins/hue/libolananoleafnode.la
 
 # TESTS
 ##################################################
-test_programs += plugins/nanoleaf/NanoleafTester
+test_programs += plugins/hue/NanoleafTester
 
-plugins_nanoleaf_NanoleafTester_SOURCES = plugins/nanoleaf/NanoleafNodeTest.cpp
+plugins_nanoleaf_NanoleafTester_SOURCES = plugins/hue/NanoleafNodeTest.cpp
 plugins_nanoleaf_NanoleafTester_CXXFLAGS = $(COMMON_TESTING_FLAGS)
 plugins_nanoleaf_NanoleafTester_LDADD = $(COMMON_TESTING_LIBS) \
-                                        plugins/nanoleaf/libolananoleafnode.la
+                                        plugins/hue/libolananoleafnode.la
 endif
 
-EXTRA_DIST += plugins/nanoleaf/README.md
+EXTRA_DIST += plugins/hue/README.md
